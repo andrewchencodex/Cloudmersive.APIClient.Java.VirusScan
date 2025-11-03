8ea9fffdcea1d87e725cee5f5331f00e87afb285
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-11-03 08:40:50 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,11 +1,13 @@
 ﻿Remove-Item –path ./client –recurse
 
-Invoke-WebRequest -Uri 'https://api.cloudmersive.com/barcode/docs/v1/swagger' -OutFile '.\barcode-api-swagger.json'
-(Get-Content .\barcode-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\barcode-api-swagger.json
-(Get-Content .\barcode-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\barcode-api-swagger.json -Encoding UTF8
+Invoke-WebRequest -Uri 'https://api.cloudmersive.com/virus/docs/v1/swagger' -OutFile '.\virus-api-swagger.json'
+(Get-Content .\virus-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\virus-api-swagger.json
+(Get-Content .\virus-api-swagger.json).replace('"http"', '"https"') | Set-Content .\virus-api-swagger.json
 
 
-java -jar ./openapi-generator-cli-7.12.0.jar generate -i .\barcode-api-swagger.json -g java -o client -c packageconfig.json
+
+
+java -jar ./openapi-generator-cli-7.12.0.jar generate -i .\virus-api-swagger.json -g java -o client -c packageconfig.json
 
 Copy-Item ./client/README.md ./README.md
 
